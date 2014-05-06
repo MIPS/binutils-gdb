c6a7579bf153cb6b77777bde7e95da895a3c7b81
Andrew Bennett <andrew.bennett@imgtec.com>
2014-05-06 15:43:13 +0100

    Fix an issue with "Rearrange MIPS INSN* masks" patch.
    This fixes an issue with Mark Shinwell's "Rearrange MIPS INSN* masks" patch
    (https://sourceware.org/ml/binutils/2007-11/msg00231.html).  In the patch
    the pref instruction had its membership flags changed from I4|I32|G3 to
    I4_32|G3.  Unfortunately G3 was defined as being I4, which made the actual
    expanded flags as: I4|I32|I4 and therefore the membership flags should have
    been I4_32.  Since the patch was committed G3 was redefined to be I4|EE.  This
    fix just removes I4 from G3 making the expanded membership flags for pref as
    I4_32|EE.
    
    ChangeLog:
    
    opcodes/
    	* mips-opc.c (G3): Remove I4.

--- opcodes/mips-opc.c
+++ opcodes/mips-opc.c
@@ -298,9 +298,7 @@ decode_mips_operand (const char *p)
 #define G2      (T3             \
                  )
 
-#define G3      (I4             \
-                 |EE            \
-                 )
+#define G3      EE
 
 /* 64 bit CPU with 32 bit FPU (single float). */
 #define SF	EE
