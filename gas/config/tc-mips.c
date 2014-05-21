3ba2805a58d2b2ffb67f7da48666d2ce54ae849c
rts <Robert.Suchanek@imgtec.com>
2014-05-21 14:32:33 +0100

    Disable check for uMIPSr6 in R_MIPS_PC18_S3 and R_MIPS_PC19_S2 relocs if pc relative addresses are aligned to 4 bytes.

--- gas/config/tc-mips.c
+++ gas/config/tc-mips.c
@@ -14857,7 +14857,7 @@ md_apply_fix (fixS *fixP, valueT *valP, segT seg ATTRIBUTE_UNUSED)
       break;
 
     case BFD_RELOC_MIPS_18_PCREL_S3:
-      if ((*valP & 0x7) != 0)
+      if ((*valP & 0x7) != 0 && !mips_opts.micromips)
 	as_bad_where (fixP->fx_file, fixP->fx_line,
 		      _("pc rel from misaligned address (%lx)"),
 		      (long) *valP);
@@ -14866,7 +14866,7 @@ md_apply_fix (fixS *fixP, valueT *valP, segT seg ATTRIBUTE_UNUSED)
       break;
 
     case BFD_RELOC_MIPS_19_PCREL_S2:
-      if ((*valP & 0x3) != 0)
+      if ((*valP & 0x3) != 0 && !mips_opts.micromips)
 	as_bad_where (fixP->fx_file, fixP->fx_line,
 		      _("pc rel from misaligned address (%lx)"),
 		      (long) *valP);
