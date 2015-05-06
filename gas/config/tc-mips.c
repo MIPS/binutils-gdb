9b2ed243caa158ca7801de44ead5a56e02d7303c
Matthew Fortune <matthew.fortune@imgtec.com>
2015-05-06 18:59:30 +0100

    Add the m6200 architecture

--- gas/config/tc-mips.c
+++ gas/config/tc-mips.c
@@ -20053,6 +20053,8 @@ static const struct mips_cpu_info mips_cpu_info_table[] =
   { "m5101",          0, ASE_MCU,		ISA_MIPS32R5, CPU_MIPS32R5 },
   /* P5600 with EVA and Virtualization ASEs, other ASEs are optional.  */
   { "p5600",          0, ASE_VIRT | ASE_EVA | ASE_XPA,	ISA_MIPS32R5, CPU_MIPS32R5 },
+  /* M6200 family */
+  { "m6201",          0, ASE_MCU,		ISA_MIPS32R6, CPU_MIPS32R6 },
 
   /* MIPS 64 */
   { "5kc",            0, 0,			ISA_MIPS64,   CPU_MIPS64 },
