025592d9a41812841d3ca7c63b334498218b48db
Faraz Shahbazker <faraz.shahbazker@imgtec.com>
2015-05-18 14:36:22 -0700

    IFUNC test-cases for mips

--- ld/testsuite/ld-mips-elf/ifunc-5-o32.r
+++ ld/testsuite/ld-mips-elf/ifunc-5-o32.r
@@ -1,1 +1,3 @@
-There are no relocations in this file.
+Relocation section '.rel.dyn' at offset 0x11000 contains 1 entries:
+ Offset     Info    Type            Sym.Value  Sym. Name
+00000810  00000080 R_MIPS_IRELATIVE 
