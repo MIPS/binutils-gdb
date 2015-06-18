7dd7e3d9d355164c2b3672dc6f41ee089bc3a1a2
Faraz Shahbazker <faraz.shahbazker@imgtec.com>
2015-06-18 11:52:59 -0700

    Modify test golden output to conform to new behaviour

--- ld/testsuite/ld-mips-elf/ifunc-5-o32.r
+++ ld/testsuite/ld-mips-elf/ifunc-5-o32.r
@@ -1,3 +1,3 @@
 Relocation section '.rel.dyn' at offset 0x11000 contains 1 entries:
  Offset     Info    Type            Sym.Value  Sym. Name
-00000810  00000080 R_MIPS_IRELATIVE 
+00000810  00000580 R_MIPS_IRELATIVE  func1\(\)    func1
