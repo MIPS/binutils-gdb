6cc586ddd2142ce41822e61e09c91bb142a52858
Faraz Shahbazker <faraz.shahbazker@imgtec.com>
2015-06-18 11:52:59 -0700

    Modify test golden output to conform to new behaviour

--- ld/testsuite/ld-mips-elf/ifunc-3-o32.t
+++ ld/testsuite/ld-mips-elf/ifunc-3-o32.t
@@ -6,5 +6,6 @@ Disassembly of section .iplt:
 00000400 <.iplt.func1>:
  400:	3c0f0000 	lui	t7,0x0
  404:	8df90800 	lw	t9,2048\(t7\)
- 408:	03200008 	jr	t9
- 40c:	00000000 	nop
+ 408:	00000000 	nop
+ 40c:	03200008 	jr	t9
+ 410:	00000000 	nop
--- ld/testsuite/ld-mips-elf/ifunc-5-n64.r
+++ ld/testsuite/ld-mips-elf/ifunc-5-n64.r
@@ -1,5 +1,5 @@
 Relocation section '.rel.dyn' at offset 0x11000 contains 1 entries:
   Offset          Info           Type           Sym. Value    Sym. Name
-000000000820  000000000080 R_MIPS_IRELATIVE 
+000000000820  000400000080 R_MIPS_IRELATIVE  func1\(\)          func1
                     Type2: R_MIPS_NONE      
                     Type3: R_MIPS_NONE      
