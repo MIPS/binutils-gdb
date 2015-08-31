67b98a6e0179c92d7ee16a007f6e1f5159191893
Faraz Shahbazker <faraz.shahbazker@imgtec.com>
2015-08-31 14:42:21 -0700

    Add tests for micromips, mips32r6, mips16; update reference outputs

--- ld/testsuite/ld-mips-elf/ifunc-5-n64.r
+++ ld/testsuite/ld-mips-elf/ifunc-5-n64.r
@@ -3,6 +3,6 @@ Relocation section '.rel.dyn' at offset 0x11000 contains 2 entries:
 000000000000  000000000000 R_MIPS_NONE      
                     Type2: R_MIPS_NONE      
                     Type3: R_MIPS_NONE      
-000000000420  000400001203 R_MIPS_REL32      func1\(\)          func1
+000000000410  000200001203 R_MIPS_REL32      func1\(\)          func1
                     Type2: R_MIPS_64        
                     Type3: R_MIPS_NONE      
