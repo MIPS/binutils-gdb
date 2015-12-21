c7074bd72c272b4f75a00b4fa42f11917be08aa0
Faraz Shahbazker <faraz.shahbazker@imgtec.com>
2015-12-21 12:02:34 -0800

    Added / revised test-cases

--- ld/testsuite/ld-mips-elf/ifunc-6-n32.r
+++ ld/testsuite/ld-mips-elf/ifunc-6-n32.r
@@ -1,4 +1,4 @@
-Relocation section '.rel.dyn' at offset 0x12000 contains 2 entries:
+Relocation section '.rel.dyn' at offset 0x11000 contains 2 entries:
  Offset     Info    Type            Sym.Value  Sym. Name
 00000000  00000000 R_MIPS_NONE      
-00000800  00000080 R_MIPS_IRELATIVE 
+00000808  00000080 R_MIPS_IRELATIVE 
