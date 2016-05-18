37e120bf94def75413bf95b08ef2d2cf5dd91301
Faraz Shahbazker <faraz.shahbazker@imgtec.com>
2016-05-18 15:28:02 -0700

    Revert "Review v4: re-enable error check for call16 against local IFUNC"
    This reverts commit c881081d475ef802087cbf5b7936c51f57f2d066.

--- bfd/elfxx-mips.c
+++ bfd/elfxx-mips.c
@@ -8942,7 +8942,8 @@ _bfd_mips_elf_check_relocs (bfd *abfd, struct bfd_link_info *info,
 	case R_MIPS_CALL16:
 	case R_MIPS16_CALL16:
 	case R_MICROMIPS_CALL16:
-	  if (h == NULL)
+	  /* Exclude local IFUNCs from check.  */
+	  if (h == NULL && ih == NULL)
 	    {
 	      (*_bfd_error_handler)
 		(_("%B: CALL16 reloc at 0x%lx not against global symbol"),
@@ -8950,8 +8951,10 @@ _bfd_mips_elf_check_relocs (bfd *abfd, struct bfd_link_info *info,
 	      bfd_set_error (bfd_error_bad_value);
 	      return FALSE;
 	    }
-	  else if (h->type == STT_GNU_IFUNC)
+	  if (h && h->type == STT_GNU_IFUNC)
 	    ((struct mips_elf_link_hash_entry *) h)->has_got_relocs = TRUE;
+	  else if (ih)
+	    ih->has_got_relocs = TRUE;
 	  /* Fall through.  */
 
 	case R_MIPS_CALL_HI16:
@@ -9042,11 +9045,6 @@ _bfd_mips_elf_check_relocs (bfd *abfd, struct bfd_link_info *info,
 	  if (h && !mips_elf_record_global_got_symbol (h, abfd, info,
 						       FALSE, r_type))
 	    return FALSE;
-
-	  if (h && h->type == STT_GNU_IFUNC)
-	    ((struct mips_elf_link_hash_entry *) h)->has_got_relocs = TRUE;
-	  else if (ih)
-	    ih->has_got_relocs = TRUE;
 	  break;
 
 	case R_MIPS_TLS_GOTTPREL:
