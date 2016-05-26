26a444561d6e1325ae06ee71c84fb543b5dcfbe0
Faraz Shahbazker <faraz.shahbazker@imgtec.com>
2016-05-26 10:27:32 -0700

    Review v4: Update tests for composite IRELATIVE relocs

--- ld/testsuite/ld-mips-elf/ifunc-4-n64.r
+++ ld/testsuite/ld-mips-elf/ifunc-4-n64.r
@@ -1,5 +1,5 @@
 Relocation section '.rel.dyn' at offset 0x11000 contains 1 entries:
   Offset          Info           Type           Sym. Value    Sym. Name
-000000080800  000000000080 R_MIPS_IRELATIVE 
-                    Type2: R_MIPS_NONE      
+000000080800  000000001280 R_MIPS_IRELATIVE 
+                    Type2: R_MIPS_64        
                     Type3: R_MIPS_NONE      
