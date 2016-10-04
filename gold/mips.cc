f180d1000f3be99d7469422183202f45b9484ad4
Vladimir Radosavljevic <vladimir.radosavljevic@imgtec.com>
2016-10-04 18:27:37 +0200

    Remove '\n' from the overflow error message.

--- gold/mips.cc
+++ gold/mips.cc
@@ -12812,7 +12812,7 @@ Target_mips<size, big_endian>::Relocate::relocate(
         {
           gold_error_at_location(relinfo, relnum, r_offset,
                                  _("relocation overflow: "
-                                 "%s against local symbol %u:\n"
+                                 "%s against local symbol %u: "
                                  "cannot encode a value of 0x%lx "
                                  "in a field of %u bits of instruction 0x%x"),
                                  overflow_info.name, r_sym, overflow_info.value,
@@ -12822,7 +12822,7 @@ Target_mips<size, big_endian>::Relocate::relocate(
         {
           gold_error_at_location(relinfo, relnum, r_offset,
                                  _("relocation overflow: "
-                                 "%s against '%s' defined in %s:\n"
+                                 "%s against '%s' defined in %s: "
                                  "cannot encode a value of 0x%lx "
                                  "in a field of %u bits of instruction 0x%x"),
                                  overflow_info.name, gsym->name(),
@@ -12834,7 +12834,7 @@ Target_mips<size, big_endian>::Relocate::relocate(
         {
           gold_error_at_location(relinfo, relnum, r_offset,
                                  _("relocation overflow: "
-                                 "%s against '%s':\n"
+                                 "%s against '%s': "
                                  "cannot encode a value of 0x%lx "
                                  "in a field of %u bits of instruction 0x%x"),
                                  overflow_info.name, gsym->name(),
