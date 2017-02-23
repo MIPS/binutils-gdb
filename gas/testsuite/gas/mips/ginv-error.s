9474c346daab7e1568865dbe23891ef898ac42ec
Faraz Shahbazker <faraz.shahbazker@imgtec.com>
2017-02-23 13:17:26 -0800

    Add Global INValidate instructions
    GINVI, GINVT: enabled by option -mginv.
    GINVGT: enabled by -mginv, -mvirt.
    Bit-17 in MIPS.abiflags is set when GINV extension is enabled.
    
    bfd/
    	* elfxx-mips.c (print_mips_ases): Add GINV extension.
    
    binutils/
    	* readelf.c (print _mis_ases) Add GINV extension.
    
    gas/
    	* config/tc-mips.c (options) <OPTION_GINV, OPTION_NOGINV>:
    	New.
    	(md_longopts): Add -m[no-]ginv options.
    	(mips_ases): Likewise.
    	(mips_set_ase): Handle combination of VX and GINV extensions.
    	(mips_convert_ase_flags): Add GINV extension.
    	(md_show_usage): Add -m[no-]ginv options.
    
    include/
    	* elf/mips.h (AFL_ASE_GINV): New.
    	(AFL_ASE_MASK): Update mask for all ASEs.
    
    	* opcode/mips.h: Comments describing new MIPS and micromips
    	operand format "+\".
    	(ASE_GINV): New.
    	(ASE_VIRT_GINV): Likewise.
    
    opcodes/
    	* mips-opc.c (decode_mips_operand) <+\>: New case.
    	(GINV): New.
    	(IVIRT_GINV): Likewise.
    	(mips_opcodes): Add instructions GINVI, GINVT, GINVGT.
    
    	* micromips-opc.c (decode_micromips_operand) <+\>: New case.
    	(GINV): New.
    	(IVIRT_GINV): Likewise.
    	(micromips_opcodes): Add instructions GINVI, GINVT, GINVGT.
    
    	* mips-dis.c (mips_arch_choices): Add GINV and VIRT_GINV ASEs
    	to mips32r6 and mips64r6 descriptors.
    	(parse_mips_dis_option): Handle -mginv option and
    	VIRT+GINV extension combination.
    
    gas/testsuite/
    	* gas/mips/mips.exp: Add new tests.
    	* gas/mips/ginv.s: New file.
    	* gas/mips/ginv-error.s: Likewise.
    	* gas/mips/ginv-virt-error.s: Likewise.
    	* gas/mips/ginv.d: Likewise.
    	* gas/mips/ginv-dis.d: Likewise.
    	* gas/mips/ginv-virt.d: Likewise.
    	* gas/mips/ginv-virt-dis.d: Likewise.
    	* gas/mips/micromipsr6@ginv.d: Likewise.
    	* gas/mips/micromipsr6@ginv-dis.d: Likewise.
    	* gas/mips/micromipsr6@ginv-virt.d: Likewise.
    	* gas/mips/micromipsr6@ginv-virt-dis.d: Likewise.
    	* gas/mips/ginv-error.l: Likewise.
    	* gas/mips/ginv-virt-error.l: Likewise.

---
+++ gas/testsuite/gas/mips/ginv-error.s
@@ -0,0 +1,8 @@
+	.text
+test:
+	ginvi	$2
+	ginvt	$3,0
+	ginvt	$4,1
+
+	.align	2
+	.space	8
---
+++ gas/testsuite/gas/mips/micromipsr6@ginv-virt-dis.d
@@ -0,0 +1,18 @@
+#objdump: -mmips:isa32 -Mginv -Mvirt -d --show-raw-insn
+#name: MIPS microMIPS GINV+VIRT instruction disassembly
+#as: --defsym VX= -mginv -mvirt
+#source: ginv.s
+
+# Check microMIPS Global INValidate Virtualization disassembly
+
+.*: +file format .*mips.*
+
+
+Disassembly of section .text:
+00000000 <test>:
+   0:	0002 617c 	ginvi	v0
+   4:	0003 717c 	ginvt	v1,0x0
+   8:	0004 737c 	ginvt	a0,0x1
+   c:	0005 7d7c 	ginvgt	a1,0x2
+  10:	0006 7f7c 	ginvgt	a2,0x3
+	\.\.\.
