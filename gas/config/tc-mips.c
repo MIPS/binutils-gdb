0b1953e87b235ad605a80a912be4876109b451bf
Faraz Shahbazker <faraz.shahbazker@imgtec.com>
2017-04-25 13:26:33 -0700

    Bugzilla 2549: Infinite loop in assembler
    *** Skip ChangeLog
    
    gas/
    	* config/tc-mips.c (relaxed_micromips_16bit_branch_length):
    	Correct condition for beqc[16] expansion.
    
    gas/testsuite/
    	* gas/mips/mips.exp: Enable relaxation test case.
    	* gas/mips/ur7-relax.s: Update test case.
    	* gas/mips/ur7-relax.d: Add reference output.

--- gas/config/tc-mips.c
+++ gas/config/tc-mips.c
@@ -19894,7 +19894,7 @@ relaxed_micromips_16bit_branch_length (fragS *fragp, asection *sec, int update)
       else if (type == 'E')
 	toofar = val < - (0x40 << 1) || val >= (0x40 << 1);
       else if (type == 'F')
-	toofar = val <= 0 || val >= 32;
+	toofar = val < -2 || val >= 32;
       else
 	abort ();
     }
--- gas/testsuite/gas/mips/ur7-relax.s
+++ gas/testsuite/gas/mips/ur7-relax.s
@@ -4,29 +4,40 @@
 	.globl	test
 	/* Relax to 16-bit */ 
 test:
-	beqc	$2,$3,test1 /* forward */
-	.space	28
+	beqc	$4,$5,test1 /* forward, relax to 32-bit */
+	.space	(1<<5)
 test1:
-	bc	test2	/* forward */
-	.space	1020
+	bc	test2	/* forward, relax to 32-bit */
+	.space	(1<<10)
 test2:
-	bc 	test1	/* backward */
-	bnec	$3, $2, test3
-	.space (1<<4)
+	bc 	test1	/* backward, relax to 32-bit */
+	bnec	$5, $4, test3
+	.space (1<<5)
 test3:	
-	beqc	$2, $3, test2
-	.space (1<<4)
+	beqc	$4, $5, test2 /* backward, relax to 32-bit */
+	.space (1<<5)
 test4:
-	balc	printf
-	.space (1<<9)
+	balc	test5 /* forward, relax to 32-bit */
+	.space (1<<10)
 	.end	test
-	.ent printf
-	.globl printf
-printf:
-	nop
-	.end printf
+	.ent test5
+	.globl test5
+test5:
+	beqc	$4,$5,test6 /* forward, no relax */
+	.space	(1<<5)-2
+test6:
+	bc	test7	/* forward, no relax */
+	.space	(1<<10)-4
+test7:
+	bc 	test6	/* backward, no relax */
+	bnec	$5, $4, test8 /* forward, no relax */
+	.space (1<<5)-2
+test8:
+	.space (1<<10)-2
+test9:
+	balc	test8 /* backward, no relax */
 
-.data
-	.globl gpdata
-gpdata:
-	.word
+	beqc $17,$7,$L1
+$L1:
+	nop
+	.end test5
