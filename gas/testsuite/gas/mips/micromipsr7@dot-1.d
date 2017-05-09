46c5325293a73888a482cf08400db06441268a12
Faraz Shahbazker <faraz.shahbazker@imgtec.com>
2017-05-09 11:15:32 -0700

    Bugzilla #2548: Relax ADDIU with label expression to correct variant
    gas/
    	* config/tc-mips.c (RELAX_MICROMIPSPP_BALC_STUB_P): New macro.
    	(RELAX_MICROMIPSPP_BRANCH_P): Likewise.
    	(RELAX_MICROMIPSPP_ADDIU_P): Likewise.
    	(RELAX_MICROMIPSPP_NEGOFF): Likewise.
    	(RELAX_MICROMIPSPP_MARK_NEGOFF): Likewise.
    	(RELAX_MICROMIPSPP_CLEAR_NEGOFF): Likewise.
    	(match_int_operand): Put relaxation token in place of reloc
    	for addiu[32].
    	(append_insn): Create relaxation frag for ADDIU.
    	(micromipspp_gpr3_reg_p): New function.
    	(relaxed_micromipspp_addiu_length): Likewise.
    	(md_estimate_size_before_relax): Handle ADDIU relaxation.
    	(mips_relax_frag): Likewise.
    	(md_convert_frag): Likewise.
    
    gas/testsuite
    	* gas/mips/relax-addiu.s: New file.
    	* gas/mips/relax-addui.d: New file.
    
    *** Skip ChangeLog:
    
    gas/testsuite/
    	* gas/mips/mips.exp: Add new test.
    	* gas/mips/micromipsr7@dot-1.d: Update reference output.
    	* gas/mips/micromipsr7@expr-diff.d: Likewise.
    	* gas/mips/isa-errors.s: Remove 64-bit cases.
    	* gas/mips/isa-errors.l: Update reference output.

--- gas/testsuite/gas/mips/micromipsr7@dot-1.d
+++ gas/testsuite/gas/mips/micromipsr7@dot-1.d
@@ -9,9 +9,9 @@ Disassembly of section \.text:
 0+0000 <foo> 9240      	addiu	a0,a0,0
 0+0002 <foo\+0x2> 9008      	nop
 0+0004 <foo\+0x4> 9008      	nop
-0+0006 <foo\+0x6> 0084 0008 	addiu	a0,a0,8
-0+000a <foo\+0xa> 908e      	addiu	a0,a0,6
-0+000c <foo\+0xc> 9243      	addiu	a0,a0,12
+0+0006 <foo\+0x6> 9242      	addiu	a0,a0,8
+0+0008 <foo\+0x8> 908c      	addiu	a0,a0,4
+0+000a <foo\+0xa> 0084 000a 	addiu	a0,a0,10
 0+000e <foo\+0xe> 0084 0000 	addiu	a0,a0,0
 0+0012 <foo\+0x12> 0084 000e 	addiu	a0,a0,14
 0+0016 <foo\+0x16> 0084 0012 	addiu	a0,a0,18
