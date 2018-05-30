1fdca9992a739dd946a57c55f31be303782f4364
Maciej W. Rozycki <macro@mips.com>
2018-05-30 13:03:41 +0100

    GT-262: GDB: Add 64-bit XML descriptions
    	gdb/
    	* features/nanomips64-cp0.xml: New file.
    	* features/nanomips64-cpu.xml: New file.
    	* features/nanomips64-dsp.xml: New file.
    	* features/nanomips64-linux.xml: New file.
    	* features/nanomips64-dsp-linux.xml: New file.
    	* features/Makefile (WHICH): Add `nanomips64-linux' and
    	`nanomips64-dsp-linux'.
    	(XMLTOC): Add `nanomips64-linux.xml' and
    	`nanomips64-dsp-linux.xml'.
    	* features/nanomips64-linux.c: Generate.
    	* features/nanomips64-dsp-linux.c: Generate.
    	* regformats/nanomips64-linux.dat: Generate.
    	* regformats/nanomips64-dsp-linux.dat: Generate.
    
    	gdb/gdbserver/
    	* configure.srv <nanomips*-*-linux*> (srv_regobj): Add
    	`nanomips64-linux.o' and `nanomips64-dsp-linux.o'.
    	<nanomips*-*-linux*> (srv_xmlfiles): Add `nanomips64-linux.xml',
    	`nanomips64-dsp-linux.xml', `nanomips64-cpu.xml',
    	`nanomips64-cp0.xml' and `nanomips64-dsp.xml'.

---
+++ gdb/features/nanomips64-dsp-linux.c
@@ -0,0 +1,110 @@
+/* THIS FILE IS GENERATED.  -*- buffer-read-only: t -*- vi:set ro:
+  Original: nanomips64-dsp-linux.xml */
+
+#include "defs.h"
+#include "osabi.h"
+#include "target-descriptions.h"
+
+struct target_desc *tdesc_nanomips64_dsp_linux;
+static void
+initialize_tdesc_nanomips64_dsp_linux (void)
+{
+  struct target_desc *result = allocate_target_description ();
+  struct tdesc_feature *feature;
+
+  set_tdesc_architecture (result, bfd_scan_arch ("nanomips"));
+
+  set_tdesc_osabi (result, osabi_from_tdesc_string ("GNU/Linux"));
+
+  feature = tdesc_create_feature (result, "org.gnu.gdb.nanomips.cpu");
+  tdesc_create_reg (feature, "r0", 0, 1, NULL, 64, "int");
+  tdesc_create_reg (feature, "r1", 1, 1, NULL, 64, "int");
+  tdesc_create_reg (feature, "r2", 2, 1, NULL, 64, "int");
+  tdesc_create_reg (feature, "r3", 3, 1, NULL, 64, "int");
+  tdesc_create_reg (feature, "r4", 4, 1, NULL, 64, "int");
+  tdesc_create_reg (feature, "r5", 5, 1, NULL, 64, "int");
+  tdesc_create_reg (feature, "r6", 6, 1, NULL, 64, "int");
+  tdesc_create_reg (feature, "r7", 7, 1, NULL, 64, "int");
+  tdesc_create_reg (feature, "r8", 8, 1, NULL, 64, "int");
+  tdesc_create_reg (feature, "r9", 9, 1, NULL, 64, "int");
+  tdesc_create_reg (feature, "r10", 10, 1, NULL, 64, "int");
+  tdesc_create_reg (feature, "r11", 11, 1, NULL, 64, "int");
+  tdesc_create_reg (feature, "r12", 12, 1, NULL, 64, "int");
+  tdesc_create_reg (feature, "r13", 13, 1, NULL, 64, "int");
+  tdesc_create_reg (feature, "r14", 14, 1, NULL, 64, "int");
+  tdesc_create_reg (feature, "r15", 15, 1, NULL, 64, "int");
+  tdesc_create_reg (feature, "r16", 16, 1, NULL, 64, "int");
+  tdesc_create_reg (feature, "r17", 17, 1, NULL, 64, "int");
+  tdesc_create_reg (feature, "r18", 18, 1, NULL, 64, "int");
+  tdesc_create_reg (feature, "r19", 19, 1, NULL, 64, "int");
+  tdesc_create_reg (feature, "r20", 20, 1, NULL, 64, "int");
+  tdesc_create_reg (feature, "r21", 21, 1, NULL, 64, "int");
+  tdesc_create_reg (feature, "r22", 22, 1, NULL, 64, "int");
+  tdesc_create_reg (feature, "r23", 23, 1, NULL, 64, "int");
+  tdesc_create_reg (feature, "r24", 24, 1, NULL, 64, "int");
+  tdesc_create_reg (feature, "r25", 25, 1, NULL, 64, "int");
+  tdesc_create_reg (feature, "r26", 26, 1, NULL, 64, "int");
+  tdesc_create_reg (feature, "r27", 27, 1, NULL, 64, "int");
+  tdesc_create_reg (feature, "r28", 28, 1, NULL, 64, "int");
+  tdesc_create_reg (feature, "r29", 29, 1, NULL, 64, "int");
+  tdesc_create_reg (feature, "r30", 30, 1, NULL, 64, "int");
+  tdesc_create_reg (feature, "r31", 31, 1, NULL, 64, "int");
+  tdesc_create_reg (feature, "pc", 32, 1, NULL, 64, "int");
+
+  feature = tdesc_create_feature (result, "org.gnu.gdb.nanomips.linux");
+  tdesc_create_reg (feature, "restart", 33, 1, "system", 64, "int");
+
+  feature = tdesc_create_feature (result, "org.gnu.gdb.nanomips.cp0");
+  tdesc_create_reg (feature, "status", 34, 1, NULL, 32, "int");
+  tdesc_create_reg (feature, "badvaddr", 35, 1, NULL, 64, "int");
+  tdesc_create_reg (feature, "cause", 36, 1, NULL, 32, "int");
+
+  feature = tdesc_create_feature (result, "org.gnu.gdb.nanomips.fpu");
+  tdesc_create_reg (feature, "f0", 37, 1, NULL, 64, "ieee_double");
+  tdesc_create_reg (feature, "f1", 38, 1, NULL, 64, "ieee_double");
+  tdesc_create_reg (feature, "f2", 39, 1, NULL, 64, "ieee_double");
+  tdesc_create_reg (feature, "f3", 40, 1, NULL, 64, "ieee_double");
+  tdesc_create_reg (feature, "f4", 41, 1, NULL, 64, "ieee_double");
+  tdesc_create_reg (feature, "f5", 42, 1, NULL, 64, "ieee_double");
+  tdesc_create_reg (feature, "f6", 43, 1, NULL, 64, "ieee_double");
+  tdesc_create_reg (feature, "f7", 44, 1, NULL, 64, "ieee_double");
+  tdesc_create_reg (feature, "f8", 45, 1, NULL, 64, "ieee_double");
+  tdesc_create_reg (feature, "f9", 46, 1, NULL, 64, "ieee_double");
+  tdesc_create_reg (feature, "f10", 47, 1, NULL, 64, "ieee_double");
+  tdesc_create_reg (feature, "f11", 48, 1, NULL, 64, "ieee_double");
+  tdesc_create_reg (feature, "f12", 49, 1, NULL, 64, "ieee_double");
+  tdesc_create_reg (feature, "f13", 50, 1, NULL, 64, "ieee_double");
+  tdesc_create_reg (feature, "f14", 51, 1, NULL, 64, "ieee_double");
+  tdesc_create_reg (feature, "f15", 52, 1, NULL, 64, "ieee_double");
+  tdesc_create_reg (feature, "f16", 53, 1, NULL, 64, "ieee_double");
+  tdesc_create_reg (feature, "f17", 54, 1, NULL, 64, "ieee_double");
+  tdesc_create_reg (feature, "f18", 55, 1, NULL, 64, "ieee_double");
+  tdesc_create_reg (feature, "f19", 56, 1, NULL, 64, "ieee_double");
+  tdesc_create_reg (feature, "f20", 57, 1, NULL, 64, "ieee_double");
+  tdesc_create_reg (feature, "f21", 58, 1, NULL, 64, "ieee_double");
+  tdesc_create_reg (feature, "f22", 59, 1, NULL, 64, "ieee_double");
+  tdesc_create_reg (feature, "f23", 60, 1, NULL, 64, "ieee_double");
+  tdesc_create_reg (feature, "f24", 61, 1, NULL, 64, "ieee_double");
+  tdesc_create_reg (feature, "f25", 62, 1, NULL, 64, "ieee_double");
+  tdesc_create_reg (feature, "f26", 63, 1, NULL, 64, "ieee_double");
+  tdesc_create_reg (feature, "f27", 64, 1, NULL, 64, "ieee_double");
+  tdesc_create_reg (feature, "f28", 65, 1, NULL, 64, "ieee_double");
+  tdesc_create_reg (feature, "f29", 66, 1, NULL, 64, "ieee_double");
+  tdesc_create_reg (feature, "f30", 67, 1, NULL, 64, "ieee_double");
+  tdesc_create_reg (feature, "f31", 68, 1, NULL, 64, "ieee_double");
+  tdesc_create_reg (feature, "fcsr", 69, 1, "float", 32, "int");
+  tdesc_create_reg (feature, "fir", 70, 1, "float", 32, "int");
+
+  feature = tdesc_create_feature (result, "org.gnu.gdb.nanomips.dsp");
+  tdesc_create_reg (feature, "hi0", 71, 1, NULL, 64, "int");
+  tdesc_create_reg (feature, "lo0", 72, 1, NULL, 64, "int");
+  tdesc_create_reg (feature, "hi1", 73, 1, NULL, 64, "int");
+  tdesc_create_reg (feature, "lo1", 74, 1, NULL, 64, "int");
+  tdesc_create_reg (feature, "hi2", 75, 1, NULL, 64, "int");
+  tdesc_create_reg (feature, "lo2", 76, 1, NULL, 64, "int");
+  tdesc_create_reg (feature, "hi3", 77, 1, NULL, 64, "int");
+  tdesc_create_reg (feature, "lo3", 78, 1, NULL, 64, "int");
+  tdesc_create_reg (feature, "dspctl", 79, 1, NULL, 32, "int");
+
+  tdesc_nanomips64_dsp_linux = result;
+}
