a9ed500b45647f9904cadce862ec7b32380ef282
Vladimir Radosavljevic <vladimir.radosavljevic@mips.com>
2018-10-23 17:45:42 +0200

    GTM18-227: PP118699: - addr2line issue (investigation)
    gold/
    	* nanomips.cc (Relocate_comdat_behavior::get): Use
    	CB_SET_TO_ONE for .debug_ranges sections.
    	(Target_nanomips::scan_reloc_section_for_transform): Handle
    	CB_SET_TO_ONE.
    	* target-reloc.h (relocate_section): Likewise.
    	(Comdat_behavior::CB_SET_TO_ONE): New enum value.

--- gold/nanomips.cc
+++ gold/nanomips.cc
@@ -1854,6 +1854,10 @@ class Target_nanomips : public Sized_target<size, big_endian>
     inline Comdat_behavior
     get(const char* name)
     {
+      // For a range list, use 1 instead of 0 as placeholder.
+      // 0 would terminate the list, hiding any later entries.
+      if (strcmp(name, ".debug_ranges") == 0)
+        return CB_SET_TO_ONE;
       if (Layout::is_debug_info_section(name))
         return CB_RETAIN;
 
@@ -6465,8 +6469,7 @@ Target_nanomips<size, big_endian>::scan_reloc_section_for_transform(
               std::string name = relobj->section_name(relinfo->data_shndx);
               comdat_behavior = default_comdat_behavior.get(name.c_str());
             }
-          if (comdat_behavior == CB_PRETEND
-              || comdat_behavior == CB_RETAIN)
+          if (comdat_behavior == CB_PRETEND || comdat_behavior == CB_RETAIN)
             {
               // FIXME: This case does not work for global symbols.
               // We have no place to store the original section index.
@@ -6482,6 +6485,10 @@ Target_nanomips<size, big_endian>::scan_reloc_section_for_transform(
                 symval2.set_output_value(psymval->input_value());
               else
                 symval2.set_output_value(0);
+            }
+          else if (comdat_behavior == CB_SET_TO_ONE)
+            {
+              symval2.set_output_value(1);
             }
           else
             {
