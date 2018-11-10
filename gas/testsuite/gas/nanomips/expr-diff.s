58172c024cccfe353242ddc9cacd5eaec7c5948d
Faraz Shahbazker <fshahbazker@wavecomp.com>
2018-11-10 15:50:46 -0800

    GTM18-283/PP118772: incorrect GDB disassembly
    When using garbage collection, AT_high_pc expressions in .debug_info
    relating to garbage collected regions can get spurious non-zero values.
    These spurious ranges overlap with retained sections positioned
    at address 0. GDB is unable to resolve such overlapping ranges and
    distinguish valid ranges from invalid ones.
    
    A way of side-stepping the issue is to let the linker resolve such
    differences to 0, when garbage-collecting sections. To enable this,
    the assembler must pass label-differences in .debug_info to the linker
    as relocations, instead of attempting to resolve them, even when
    such relocations are link-time invariable.
    
    gas/
    	* config/tc-nanomips.c (frag_in_section_p): New.
    	(nanomips_force_relocation): Force label-difference relocations
    	in .debug_info section.
    	(nanomips_allow_local_subtract): Likewise.
    	(nanomips_validate_fix_sub): Likewise.
    	* testsuite/gas/nanomips/expr-diff.s: Modify test source for coverage.
    	* testsuite/gas/nanomips/expr-diff.d: Modify test case for coverage.

--- gas/testsuite/gas/nanomips/expr-diff.s
+++ gas/testsuite/gas/nanomips/expr-diff.s
@@ -1,6 +1,8 @@
 # Generated from C code - pr70460.c in gcc.torture.  This is the initial
 # case that highlights the need for symbol-differences as relocations
 # when doing linker relaxations.
+# Extended to check that label-differences in .debug_info will always
+# have relocations, even when the difference is link-time invariant.
 	.text
 	.linkrelax
 	.ent foo
@@ -37,21 +39,31 @@ foo2:
 	jr	$r4
 .L7:
 	lw	$r4,4($r28)
-	addiu	$r4,$r4, .L6-(.L7)
 	sw	$r4,0x100($r28)
+.L10:
+	addiu	$r4,$r4, .L6-(.L7)
 	.align 2 # Causes a frag to be generated
 .L8:
-	lw	$r4,%gprel(foo)($r28)
 	addiu	$r4,$r4,1
-	.reloc .L6, R_NANOMIPS_JALR16, foo # explicit relocation
+	lw	$r4,%gprel(foo)($r28)
 .L6:
+	.reloc .L6, R_NANOMIPS_JALR16, foo # explicit relocation
 	jalr	$r31
 .L9:
 	.end foo2
+
 	.data
 	# No intervening relocations, but align directive creates new frag
 	.word	.L7-(.L8)
 	# No intervening relocations, difference is fixed.
-	.word	.L6-(.L8)
+	.word	.L10-(.L7)
+	# Explicit relocation causes difference to be variable.
+	.word	.L9-(.L6)
+
+	.section .debug_info,"",@progbits
+	# No intervening relocations, but align directive creates new frag
+	.word	.L7-(.L8)
+	# Difference is fixed, but must be relocated within .debug_info
+	.word	.L10-(.L7)
 	# Explicit relocation causes difference to be variable.
 	.word	.L9-(.L6)
