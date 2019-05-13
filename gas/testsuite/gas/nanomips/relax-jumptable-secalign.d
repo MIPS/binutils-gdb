1721c039c2e2574ae5050b7e7f9695b451aee698
Faraz Shahbazker <fshahbazker@wavecomp.com>
2019-05-13 13:15:36 -0700

    PP128987/GTM19-437: Fix section alignment after relaxation
    gas/
    	* config/tc-nanomips.c (md_convert_frag): Adjust section
    	alignment to be at least the size of jump vectors.
    	* testsuite/gas/nanomips/relax-jumptable-secalign.d: New test.
    	* testsuite/gas/nanomips/relax-jumptable-secalign.s: New
    	test source.
    	* testsuite/gas/nanomips/nanomips.exp: Run the new test.

---
+++ gas/testsuite/gas/nanomips/relax-jumptable-secalign.d
@@ -0,0 +1,12 @@
+#PROG: readelf
+#readelf: -S
+#name: Jump-table relaxation with section alignment adjustment
+
+# Check section alignment after jumptable relaxation.
+
+There are [0-9]+ section headers, starting at offset 0x.*
+
+Section Headers:
+#...
+ +\[ *[0-9]+\] \.rodata +PROGBITS +00000000 [0-9a-f]+ [0-9a-f]+ 00 +A +0 +0 +4
+#pass
